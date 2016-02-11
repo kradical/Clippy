6355c6c219bf6d19eab4d3a978b104066ae1a9bc
Konrad Schultz <Konrad Schultz>
2016-02-10 18:13:46 -0800

    convert tabs to spaces

--- js/clippy.js
+++ js/clippy.js
@@ -3,57 +3,57 @@ var blackList = ['', 'A', 'TO', 'THE']
 var currentWord = '';
 
 (function loadImage() {
-	var clippyImgURL = chrome.extension.getURL("images/clippy.gif");
-	var html = '<img class="clippy_imageGIF" src="' + clippyImgURL + '"/>' +
-			   '<div class="clippy_textContainer">' +
-			     '<div class="clippy_speechBubble">'+
-			       '<div class="clippy_speechBubbleTip"></div>' +
-			       '<div class="clippy_text"></div>' +
-			       '<div class="clippy_textCancelButton">Cancel</div>' +
-			     '</div>' +
-			   '</div>'
-	$("body").append(html);
+    var clippyImgURL = chrome.extension.getURL("images/clippy.gif");
+    var html = '<img class="clippy_imageGIF" src="' + clippyImgURL + '"/>' +
+               '<div class="clippy_textContainer">' +
+                 '<div class="clippy_speechBubble">'+
+                   '<div class="clippy_speechBubbleTip"></div>' +
+                   '<div class="clippy_text"></div>' +
+                   '<div class="clippy_textCancelButton">Cancel</div>' +
+                 '</div>' +
+               '</div>'
+    $("body").append(html);
 })();
 
 function handleKeydown(e) {
-	if (e.which >= 65 && e.which <= 90 ) {
-		currentWord += String.fromCharCode(e.which)
+    if (e.which >= 65 && e.which <= 90 ) {
+        currentWord += String.fromCharCode(e.which)
 
-	} else if(e.which === 8){
-		currentWord = currentWord.slice(0,currentWord.length-1);
-	} else {
-		if (keyWords.length > 10) {
-			keyWords.shift()
-		}
-		if (!($.inArray(currentWord, blackList) > -1)) {
-			keyWords.push(currentWord);
-		}
-		currentWord = '';
-	}
+    } else if(e.which === 8){
+        currentWord = currentWord.slice(0,currentWord.length-1);
+    } else {
+        if (keyWords.length > 10) {
+            keyWords.shift()
+        }
+        if (!($.inArray(currentWord, blackList) > -1)) {
+            keyWords.push(currentWord);
+        }
+        currentWord = '';
+    }
 }
 
 function makeSuggestion() {
-	var clippyText;
-	if(keyWords.length === 0) {
-		clippyText = "Hi! I am Clippy, your office assistant. Would you like some assistance today?";
-	} else {
-		clippyText = keyWords.join(' ');
-		$(".clippy_textCancelButton").show();
-	}
-	$(".clippy_text").text(clippyText);
-	$(".clippy_textContainer").show();
+    var clippyText;
+    if(keyWords.length === 0) {
+        clippyText = "Hi! I am Clippy, your office assistant. Would you like some assistance today?";
+    } else {
+        clippyText = keyWords.join(' ');
+        $(".clippy_textCancelButton").show();
+    }
+    $(".clippy_text").text(clippyText);
+    $(".clippy_textContainer").show();
 }
 
 window.setInterval(function() {
-	makeSuggestion();
+    makeSuggestion();
 }, 5000)
 
 $(document).on("keydown", function(e){
-	handleKeydown(e);
+    handleKeydown(e);
 });
 
 $(".clippy_textCancelButton").on('click', function() {
-	keyWords = [];
-	$(".clippy_textCancelButton").hide();
-	$(".clippy_textContainer").hide();
+    keyWords = [];
+    $(".clippy_textCancelButton").hide();
+    $(".clippy_textContainer").hide();
 });
